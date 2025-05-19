fcbebd63fb79ba67bc7672c3b7b4f8a7cd0c42bd
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-19 16:45:07 -0600

    logica de desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { menuItems } from './data/db'
+import useOrder from './hooks/useOrder'
+import MenuItem from './components/MenuItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { addItem } = useOrder()
 
   return (
     <>
      <header className='bg-teal-400 py-5'>
         <h1 className='text-center text-4xl font-black'>CALCULADORA DE PROPINAS Y CONSUMO</h1>
      </header>
+
+     <main className='max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
+      <div className='p-5'>
+        <h2 className='text-4xl font-black'>MENÚ</h2>
+
+        <div className='space-y-3 mt-10'>
+        {menuItems.map(item =>(
+          <MenuItem 
+            key={item.id}
+            item={item}
+            addItem={addItem}
+          />
+        ))}
+        </div>
+      </div>
+      <div>
+        <h2>CONSUMO</h2>
+      </div>
+     </main>
+    
     </>
   )
 }
